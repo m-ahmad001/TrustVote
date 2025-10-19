4618a94cf4b817e2f5c3b2a9c08faf63aaf69850
Muhammad Ahmad <mahmadamir2001@gmail.com>
2025-10-19 21:07:45 +0500

    Refactor project structure: consolidate components into Home directory and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import React from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import HowItWorks from "./components/HowItWorks";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="bg-dark font-display text-white">
-      <div className="relative flex h-auto min-h-screen w-full flex-col overflow-x-hidden">
-        <div className="layout-container flex h-full grow flex-col">
-          <div className="px-4 md:px-10 lg:px-20 xl:px-40 flex flex-1 justify-center py-5">
-            <div className="layout-content-container flex flex-col max-w-[1200px] flex-1">
-              <Header />
-              <main className="flex-1">
-                <Hero />
-                <About />
-                <HowItWorks />
-                <Features />
-              </main>
-              <Footer />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Home />
     </div>
   );
 }
